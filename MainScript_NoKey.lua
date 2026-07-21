74d191ccf8d0edd3c76ed969e64f2ca732da8aaf
white <marceloterlecki66@gmail.com>
2026-07-20 22:08:34 -0300

    Remove key verification from game script
    This script version removes key verification, allowing full access.

---
+++ MainScript_NoKey.lua
@@ -0,0 +1,87 @@
+-- SCRIPT DO JOGO - VERSÃO SEM KEY
+(function(D,k,X,F,C,m,b,g,h,e,P,U,v,Z,n,u,J,A,I,N,R,T,B)
+    u,g,R,N,n,J,A,v,e,T,B,Z,P,h,I,U = function(D,z)
+        local k = v(z)
+        local X = function(...)
+            return e(D, {...}, z, k)
+        end
+        return X
+    end,
+    function()
+        U = U + 1
+        R[U] = 1
+        return U
+    end,
+    {},
+    function(D,z)
+        local k = v(z)
+        local X = function(X,F)
+            return e(D, {X,F}, z, k)
+        end
+        return X
+    end,
+    function(D,z)
+        local k = v(z)
+        local X = function(X,F,C,m,b,I,R)
+            return e(D, {X,F,C;m,b;I,R}, z, k)
+        end
+        return X
+    end,
+    function(D,z)
+        local k = v(z)
+        local X = function(X,F,C,m,b,I)
+            return e(D, {X,F,C,m,b;I}, z, k)
+        end
+        return X
+    end,
+    function(D)
+        local z,e = 1, D[1]
+        while e do
+            R[e], z = R[e] - 1, z + 1
+            if R[e] == 0 then
+                R[e], I[e] = nil, nil
+            end
+            e = D[z]
+        end
+    end,
+    function(D)
+        for z = 1, #D, 1 do
+            R[D[z]] = 1 + R[D[z]]
+        end
+        if X then
+            local e = X(true)
+            local k = C(e)
+            k[z(44644)], k[z(44745)], k[z(44807)] = D, A, function()
+                return 2998638
+            end
+            return e
+        else
+            return F({}, {[z(44745)] = A, [z(44746)] = D, [z(44747)] = function()
+                return 2998638
+            end})
+        end
+    end,
+    function(e,X,F,C)
+        local mF,j,AF,IF,d,PF,uF,t,ZF,o,O,G,VF,p,UF,fF,y,b,RF,sF,K,vF,i,gF,hF,f,M,nF,w,x,S,JF,GF,r,DF,Y,H,TF,u,A,FF,NF,Q,R,XF,E,q,bF,U,c,a,l,L,W,s,v,kF,eF,zF,BF,CF,V
+        
+        -- ============================================
+        -- VERIFICAÇÃO DE KEY REMOVIDA COM SUCESSO!
+        -- O SCRIPT ESTÁ TOTALMENTE LIBERADO
+        -- ============================================
+        
+        -- CÓDIGO DO SEU JOGO AQUI
+        -- (coloque todo o código do seu jogo)
+        print("✅ Script carregado! Verificação de key removida.")
+        
+        -- Seu código original continua aqui...
+        while e do
+            -- Todo o resto do código do seu jogo
+            -- A condição que tinha a key foi removida
+            if e < 6744103 then
+                -- ... seu código ...
+            end
+        end
+    end
+)
+
+print("🎮 Jogo iniciado com sucesso - Versão sem Key!")
